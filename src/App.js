3351d44c206dab7ae8dfb23b291df2c2485c73ca
HanaRsm <h.alrassam@live.com>
2018-08-06 17:59:22 +0300

    now it's completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
+import authors from "./data";
 
 class App extends Component {
   render() {
+    console.log(authors);
     return (
       <div id="app" className="container-fluid">
         <div className="row">
@@ -24,15 +26,17 @@ class App extends Component {
                     <div className="image">
                       <img
                         className="card-img-top img-fluid"
-                        src="https://nation.com.pk/digital_images/large/2015-05-10/jk-rowling-believes-in-standing-up-to-bullies-1431261222-3596.jpeg"
-                        alt="J.K. Rowling"
+                        src={authors[0].imageUrl}
+                        alt={authors[0].first_name + " " + authors[0].last_name}
                       />
                     </div>
                     <div className="card-body">
                       <h5 className="card-title">
                         <span>J.K. Rowling</span>
                       </h5>
-                      <small className="card-text">7 books</small>
+                      <small className="card-text">
+                        {authors[0].books.length} book
+                      </small>
                     </div>
                   </div>
                 </div>
@@ -41,15 +45,17 @@ class App extends Component {
                     <div className="image">
                       <img
                         className="card-img-top img-fluid"
-                        src="https://typeset-beta.imgix.net/elite-daily/2016/03/06144207/elite-daily-George-RR-Martin.jpg?w=800&h=800&auto=format&q=70&fit=crop&crop=faces"
-                        alt="George R.R. Martin"
+                        src={authors[1].imageUrl}
+                        alt={authors[1].first_name + " " + authors[1].last_name}
                       />
                     </div>
                     <div className="card-body">
                       <h5 className="card-title">
                         <span>George R.R. Martin</span>
                       </h5>
-                      <small className="card-text">5 books</small>
+                      <small className="card-text">
+                        {authors[1].books.length} book
+                      </small>
                     </div>
                   </div>
                 </div>
@@ -58,15 +64,17 @@ class App extends Component {
                     <div className="image">
                       <img
                         className="card-img-top img-fluid"
-                        src="https://images.8tracks.com/cover/i/010/010/920/tolkeinbigger-2362.jpg?rect=166,0,500,500&q=98&fm=jpg&fit=max"
-                        alt="J.R.R Tolkein"
+                        src={authors[2].imageUrl}
+                        alt={authors[2].first_name + " " + authors[2].last_name}
                       />
                     </div>
                     <div className="card-body">
                       <h5 className="card-title">
                         <span>J.R.R Tolkein</span>
                       </h5>
-                      <small className="card-text">6 books</small>
+                      <small className="card-text">
+                        {authors[1].books.length} book
+                      </small>
                     </div>
                   </div>
                 </div>
